9137fba529a133c4065555625a84fcbf6572d62b
adarsh maddheshiya <adarshgan78@gmail.com>
2023-08-23 21:52:24 +0530

    Conversation Page Setup Debugging 2

--- bhejoapp/src/templates/app/Conversation.jsx
+++ bhejoapp/src/templates/app/Conversation.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../../assets/css/style.css'
-import '../../assets/js/script.js'
-importScripts('../../assets/js/script.js')
+import script from '../../assets/js/script.js'
 const Conversation = () => {
   return (
     <div>
@@ -145,6 +144,7 @@ const Conversation = () => {
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src={script}></script>
     </div>
   )
 }
