563273b1ef528ae3da43b3069a595c703ca524af
adarsh maddheshiya <adarshgan78@gmail.com>
2023-08-23 21:56:33 +0530

    Conversation Page Setup Debugging 3

--- bhejoapp/src/templates/app/Conversation.jsx
+++ bhejoapp/src/templates/app/Conversation.jsx
@@ -143,8 +143,7 @@ const Conversation = () => {
     </section>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <script src={script}></script>
+    {/* <script src={script}></script> */}
     </div>
   )
 }
