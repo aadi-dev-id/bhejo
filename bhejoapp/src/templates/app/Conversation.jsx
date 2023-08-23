555b7b1b3f400d3e920ae6a9aec8da922802cc28
adarsh maddheshiya <adarshgan78@gmail.com>
2023-08-23 22:24:37 +0530

    Debugging 3

--- bhejoapp/src/templates/app/Conversation.jsx
+++ bhejoapp/src/templates/app/Conversation.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../assets/css/convers.css'
-importScripts('../../assests/js/script.js');
+import script from '../../assests/js/script.js'
 const Conversation = () => {
   return (
     <div>
@@ -143,7 +143,7 @@ const Conversation = () => {
     </section>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-    {/* <script src={script}></script> */}
+    <script src={script}></script>
     </div>
   )
 }
